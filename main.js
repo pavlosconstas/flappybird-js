4a68371619f900efe1a177d2dc22a93491fd0c75
Pavlos Constas <pconstasm@gmail.com>
2021-08-05 16:58:32 -0400

    bird and background

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ var ctx = canvas.getContext("2d")
 
 document.addEventListener("keydown", birdMove)
 
-birdX = 20
+birdX = 50
 birdY = 300
 
 var bird = new Image()
@@ -12,22 +12,29 @@ var topPipe = new Image()
 var bottomPipe = new Image()
 
 bird.src = "images/bird.png"
-background.src = "images/topPipe.png"
+background.src = "images/background.png"
 topPipe.src = "images/topPipe.png"
 bottomPipe.src = "images/bottomPipe.png"
 
+function gravity() {
+    if (birdY < 755) {
+        birdY += 1
+    }
+}
 
 function birdMove() {
-    if (birdY > 0){ 
-        birdY -= 20
+    if (birdY > 10){ 
+        birdY -= 10
     }
     console.log(birdY)
 }
 
 function draw() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height)
     ctx.drawImage(background,0, 0)
+    ctx.drawImage(bird,birdX, birdY)
+    ctx.drawImage(pipe, 50, -800)
 }
 
-background.onload = function() {
-    draw()
-}
+setInterval(gravity, 10)
+setInterval(draw, 1000/60)
