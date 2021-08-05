75edd455a320772224aff55c095e80107e914689
Pavlos Constas <pconstasm@gmail.com>
2021-08-04 20:21:14 -0400

    need to add images,

--- main.js
+++ main.js
@@ -6,7 +6,28 @@ document.addEventListener("keydown", birdMove)
 birdX = 20
 birdY = 300
 
+var bird = new Image()
+var background = new Image()
+var topPipe = new Image()
+var bottomPipe = new Image()
+
+bird.src = "images/bird.png"
+background.src = "images/topPipe.png"
+topPipe.src = "images/topPipe.png"
+bottomPipe.src = "images/bottomPipe.png"
+
+
 function birdMove() {
-    birdY += 20
+    if (birdY > 0){ 
+        birdY -= 20
+    }
     console.log(birdY)
+}
+
+function draw() {
+    ctx.drawImage(background,0, 0)
+}
+
+background.onload = function() {
+    draw()
 }
